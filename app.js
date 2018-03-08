0c7a0d420d68802e0a6e74eeca1161832dba656f
Németh Tamás <tomuwhu@gmail.com>
2018-03-08 10:04:29 +0100

    Új app: Sprortmérközés napló

--- app.js
+++ app.js
@@ -12,28 +12,17 @@ app .use(bodyParser.json()).use(cookieParser())
 mongoose.connect('mongodb://localhost/'+dbname, { useMongoClient: true })
 mongoose.Promise = global.Promise
 
-var üzenet_a_szerverről = `Heló világ`
-
 app.get( '/', (req, res) => res.sendFile('index.html', frontend) )
+
 app.get( '/backend', (req, res) => res.sendFile('main.html', frontend) )
-app.get( '/backend/:id', (req, res) => {
-    vt = ['Heló világ','Másik üzenet','','Cica','Mókus','Harmadik üzi']
-    if (req.params.id == 2) res.sendFile('main.html', frontend)
-    else res.send(`
-        <div class="w3-center">
-          <br><i>Péda back-end templating:</i> <b>${vt[req.params.id%6]}!</b><br>
-          <br><i>Péda back-end templating + AJAX:</i> <span ng-include="'/incl'"></span><hr>
-          Front-end templating:<br><br>
-          <input ng-model='x' />
-          <br><hr>{{x-1}} - {{x}} - {{x-1+2}}<br><hr>
-          Kiválasztott menüpont (vegyes templating):
-          <b>{{menu[${req.params.id}]}}</b>
-          <br><hr><br>
-        </div>
-    `)
-})
-app.get( '/incl', (req, res) => {
-    szerverről = "Ez"
-    res.send(`<b>${szerverről} AJAX.</b>`)
-})
+
+app.get( '/backend/ujcsapat', (req, res) => res.sendFile('csapat.html', frontend) )
+
+
+
+app.post( '/backend/csapatment', (req, res) => {
+    console.log(req.body)
+    res.send({ok: 1})
+} )
+
 app.listen(3001)
--- public/index.js
+++ public/index.js
@@ -1,12 +1,24 @@
-menu = [ 'Delfin', 'Kutya','Farkas', 'Pingvin','Hello világ!', 'Krokodil' ].sort()
 angular
   .module( 'a', ['ngRoute'] )
   .config($routeProvider => {
-      menu.forEach( (v, k) => $routeProvider.when('/'+k, { templateUrl : "/backend/"+k}) )
-      $routeProvider.when("/", { templateUrl : "/backend" })
+      $routeProvider.when('/ujcsapat', { templateUrl : "/backend/ujcsapat"})
+      $routeProvider.when('/', { templateUrl : "/backend" })
   })
   .controller( 'c', ($scope,$http,$interval) => {
       $interval( () => $scope.time = new Date(), 100)
-      $scope.menu = menu
-      $scope.x = 5
+      kezdomezszam = 1
+      $scope.csapat = {
+          nev : '',
+          jatekosok : []
+      }
+      $scope.uj = () => {
+            $scope.csapat.jatekosok.push({
+                mezszam: kezdomezszam++,
+                nev: ''
+            })
+      }
+      $scope.kuld = () => {
+            $http
+              .post("/backend/csapatment", $scope.csapat)
+      }
   } )
