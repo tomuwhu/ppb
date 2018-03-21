c8682b75392a95e1ebdc71e5d48a235b34ba2a37
Tamás Németh <tomuwhu@gmail.com>
2018-03-21 11:49:54 +0100

    s

--- public/index.js
+++ public/index.js
@@ -1,10 +1,9 @@
-
 angular
-  .module( 'a', ['ngRoute','ngMaterial','ngAnimate','ngAria','ngMessages'] )
+  .module( 'a', ['ngRoute','ngMaterial','ngMessages'] )
   .config($routeProvider => {
       $routeProvider.when("/", { templateUrl : "/be" })
       $routeProvider.when("/pr", { templateUrl : "/pr" })
-      $routeProvider.when("/reg", { templateUrl : "/regform" })      
+      $routeProvider.when("/reg", { templateUrl : "/regform" })
   })
   .controller( 'c', ($scope, $http, $interval) => {
       $interval( () => $scope.time = new Date(), 100)
