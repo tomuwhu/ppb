9d18bbcb700cd2d491294f7dda0c4db3ce1ce4ca
Németh Tamás <tomuwhu@gmail.com>
2018-03-26 11:19:52 +0200

    alap

--- public/index.js
+++ public/index.js
@@ -1,12 +1,9 @@
-menu = [ 'Delfin', 'Kutya','Farkas', 'Pingvin','Hello világ!', 'Krokodil' ].sort()
 angular
   .module( 'a', ['ngRoute'] )
   .config($routeProvider => {
-      menu.forEach( (v, k) => $routeProvider.when('/'+k, { templateUrl : "/backend/"+k}) )
       $routeProvider.when("/", { templateUrl : "/backend" })
+      $routeProvider.when("/main", { templateUrl : "/backend" })
   })
   .controller( 'c', ($scope,$http,$interval) => {
       $interval( () => $scope.time = new Date(), 100)
-      $scope.menu = menu
-      $scope.x = 5
   } )
